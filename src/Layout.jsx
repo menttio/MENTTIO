edf7167f803a267406bcbeacdba5b5df11bcb624
Raul <raulclasespart@gmail.com>
2025-12-19 10:04:33 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -124,7 +124,7 @@ export default function Layout({ children, currentPageName }) {
     );
   }
 
-  if (currentPageName === 'SelectRole') {
+  if (currentPageName === 'SelectRole' || currentPageName === 'TeacherSignup') {
     return <div className="min-h-screen bg-[#f2f2f2]">{children}</div>;
   }
 
