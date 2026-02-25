58a97468c8ffea366da9b99b9dea86359d350d9b
Raul <raulclasespart@gmail.com>
2026-02-25 13:25:00 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -77,11 +77,12 @@ import TeacherCalendar from './pages/TeacherCalendar';
 import TeacherClassHistory from './pages/TeacherClassHistory';
 import TeacherDashboard from './pages/TeacherDashboard';
 import TeacherProfile from './pages/TeacherProfile';
-import TeacherSignup from './pages/TeacherSignup';
-import TeacherSignupComplete from './pages/TeacherSignupComplete';
 import TeacherWorkload from './pages/TeacherWorkload';
 import TermsOfService from './pages/TermsOfService';
 import UserNotRegistered from './pages/UserNotRegistered';
+import TeacherSignup from './pages/TeacherSignup';
+import TeacherSignupComplete from './pages/TeacherSignupComplete';
+import TeacherSignupPayment from './pages/TeacherSignupPayment';
 import __Layout from './Layout.jsx';
 
 
@@ -116,11 +117,12 @@ export const PAGES = {
     "TeacherClassHistory": TeacherClassHistory,
     "TeacherDashboard": TeacherDashboard,
     "TeacherProfile": TeacherProfile,
-    "TeacherSignup": TeacherSignup,
-    "TeacherSignupComplete": TeacherSignupComplete,
     "TeacherWorkload": TeacherWorkload,
     "TermsOfService": TermsOfService,
     "UserNotRegistered": UserNotRegistered,
+    "TeacherSignup": TeacherSignup,
+    "TeacherSignupComplete": TeacherSignupComplete,
+    "TeacherSignupPayment": TeacherSignupPayment,
 }
 
 export const pagesConfig = {
