532a2edfaefdfc88eb3aba45839b7dd58d2dede1
Raul <raulclasespart@gmail.com>
2026-03-21 11:14:05 +0000

    File changes

--- src/components/booking/BookingCard.jsx
+++ src/components/booking/BookingCard.jsx
@@ -117,9 +117,7 @@ export default function BookingCard({
     setUploading(true);
     try {
       const uploadPromises = files.map(async (file) => {
-        const formData = new FormData();
-        formData.append('file', file);
-        const response = await base44.functions.invokeRaw('uploadBookingFile', formData);
+        const response = await base44.functions.invoke('uploadBookingFile', { file });
         return response.data;
       });
       const results = await Promise.all(uploadPromises);
